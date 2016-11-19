933c367bb179f1ccf92c5b689128d771f1364a25
m0rphism <m0rphism@users.noreply.github.com>
2016-11-19 14:07:07 +0100

    Update.

--- src/DMenu.hs
+++ src/DMenu.hs
@@ -74,21 +74,30 @@ import DMenu.Run
 
   The @dmenu@ command line utility
 
-  1.  takes @"Options"@ as arguments and reads a list of strings from @stdin@,
+  1.  takes @Options@ as arguments and reads a list of strings from @stdin@,
   2.  presents the list in a special overlay window, in which the user can select
       from the list via fuzzy matching, and
   3.  prints the selected string to @stdout@ or fails with exit code @1@ if the
       user hit the @ESC@ key.
 
+  Typical uses of @dmenu@ are for example
+
+  1.  as a program launcher by piping the program names from @PATH@ into @dmenu@
+      and executing the selected program.
+  2.  as an interface for killing programs by piping process information from
+      @ps aux@ into @dmenu@, and running @kill -9@ on the selected process id.
+  3.  as an interface for mounting devices by piping the device files from @\/dev\/@
+      into @dmenu@, and running @pmount@ on the selected device.
+
   @dmenu2@ is a fork of @dmenu@, which provides additional options covered in
   the @Options2@ data type.
 
-  This library extends the functionality of dmenu, by reading a set of default
-  command line options from a configuration file if existent.
-
+  Additionally to the functionality of @dmenu@ and @dmenu2@, this library
+  supports specifying default command line options for @dmenu@ in a configuration
+  file.
 
   The following example uses this library to let the user choose between the
-  strings @"A"@, @"B"@, and @"C"@.
+  strings @\"A\"@, @\"B\"@, and @\"C\"@.
 
   > import qualified DMenu
   >
